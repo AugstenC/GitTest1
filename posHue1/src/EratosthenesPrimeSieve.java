c5c1799f290c402d8a9bf5271227182cc69189ad
AugstenC <AugstenC22036@sus.htl-grieskirchen.at>
2024-09-17 15:04:23 +0200

    made code right and more readable

---
+++ posHue1/src/EratosthenesPrimeSieve.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EratosthenesPrimeSieve implements PrimeSieve {
+    private int limit;
+    public EratosthenesPrimeSieve(int limit) {
+        this.limit = limit;
+    }
+    // private List<Integer> primalNumbers = new ArrayList<Integer>();
+    @Override
+    public boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n == 2 || n == 3) {
+            return true;
+        }
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+
+        int limit = (int) Math.sqrt(n);
+        for (int i = 5; i <= limit; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    @Override
+    public void printPrimes() {
+        for (int i = 0; i < this.limit; i++) {
+            boolean b = isPrime(i);
+            if(b){
+                System.out.println(i);
+            }
+        }
+    }
+}
--- posHue1/src/Main.java
+++ posHue1/src/Main.java
@@ -1,22 +1,13 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public interface PrimeSieve {
-// U e b e r p r u ef t ob p ei n e Primzahl i s t ode r ni c h t .
-        public static boolean isPrime(int p){
-            if(p == 0||p == 1 || p == 2){
-                return true;
-            }
-            for(int i = 2; i <= Math.sqrt(p); i++){
-                if(p % i == 0){
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-            return false;
-        }
-// Gibt a l l e Primzahlen b i s zu r Obergrenze auf de r Konsole aus .
-        public void printPrimes();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a limit for the number of to where the primal numbers should be reaching");
+        EratosthenesPrimeSieve e = new EratosthenesPrimeSieve(Integer.parseInt(sc.nextLine()));
+        e.printPrimes();
+        sc.close();
     }
 }
